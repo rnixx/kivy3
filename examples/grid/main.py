25c6c10977357f510d8abf35d478aae09f0cb740
shaunbarlow <shaunbarlowmusic@gmail.com>
2019-12-25 00:08:52 +1100

    GridGeometry() generator complete. Creates a grid on the xy plane centered at origin. Completed grid example. Included notes about fragment shader not being called until triangles are added to scene.

--- examples/grid/main.py
+++ examples/grid/main.py
@@ -7,14 +7,53 @@ from kivy.clock import Clock
 from kivy3 import Scene, Renderer, PerspectiveCamera, Geometry, Vector3, Material, Mesh, Face3
 from kivy3.core.line2 import Line2
 from kivy3.extras.geometries import BoxGeometry
+from kivy3.extras.grid import GridGeometry
 from kivy3.loaders import OBJLoader
 from kivy.uix.floatlayout import FloatLayout
-
-# Resources pathes
 from kivy3.objects.lines import Lines
 
-_this_path = os.path.dirname(os.path.realpath(__file__))
-obj_file = os.path.join(_this_path, "./MQ-27.obj")
+
+class MainApp(App):
+
+    def build(self):
+        self.renderer = Renderer()
+        scene = Scene()
+        camera = PerspectiveCamera(45, 1, 0.1, 2500)
+        self.renderer.set_clear_color((.2, .2, .2, 1.))
+
+        self.camera = camera
+        root = ObjectTrackball(camera, 10)
+
+        # add a cube to the environment as an example
+        # NOTE: the grid will be rendered without transparency if it
+        # is added before the box.
+        # This may be because the shader is not called until a 'triangles' mesh is
+        # rendered? Hence the Fragment Shader has not yet been called?
+        geometry = BoxGeometry(1, 1, 1)
+        material = Material(color=(1., 1., 1.), diffuse=(1., 1., 1.),
+                            specular=(.35, .35, .35))
+        obj = Mesh(geometry, material)
+        scene.add(obj)
+
+        # create a grid on the xz plane
+        geometry = GridGeometry(size=(30, 30), spacing=1)
+        material = Material(color=(1., 1., 1.), diffuse=(1., 1., 1.),
+                            specular=(.35, .35, .35), transparency=.1)
+        lines = Lines(geometry, material)
+        lines.rotation.x = 90
+        scene.add(lines)
+
+        self.renderer.render(scene, camera)
+        self.renderer.main_light.intensity = 500
+
+        root.add_widget(self.renderer)
+        self.renderer.bind(size=self._adjust_aspect)
+        return root
+
+    def _adjust_aspect(self, inst, val):
+        rsize = self.renderer.size
+        aspect = rsize[0] / float(rsize[1])
+        self.renderer.camera.aspect = aspect
 
 
 class ObjectTrackball(FloatLayout):
@@ -61,58 +100,6 @@ class ObjectTrackball(FloatLayout):
         y = self.radius * math.cos(_phi)
         self.camera.pos = x, y, z
         self.camera.look_at((0, 0, 0))
-
-
-class MainApp(App):
-
-    def build(self):
-        self.renderer = Renderer()
-        scene = Scene()
-        camera = PerspectiveCamera(45, 1, 0.1, 2500)
-        self.renderer.set_clear_color((.2, .2, .2, 1.))
-        # loader = OBJLoader()
-        # obj = loader.load(obj_file)
-        # self.obj3d = obj
-        self.camera = camera
-        root = ObjectTrackball(camera, 10)
-
-        # scene.add(obj)
-
-        geometry = BoxGeometry(1, 1, 1)
-        material = Material(color=(1., 1., 1.), diffuse=(1., 1., 1.),
-                            specular=(.35, .35, .35))
-        obj = Mesh(geometry, material)
-
-        scene.add(obj)
-
-        geometry = Geometry()
-        d = 1.
-        l = 4.
-        for i in range(5):
-            geometry.vertices.append(Vector3(i * d, 0., 0.))
-            geometry.vertices.append(Vector3(i * d, l, 0.))
-            geometry.lines.append(Line2(a=i*2, b=((i*2) + 1)))
-        for i in range(5):
-            geometry.vertices.append(Vector3(0., i * d, 0.))
-            geometry.vertices.append(Vector3(l, i * d, 0.))
-            geometry.lines.append(Line2(a=(i + 5) * 2, b=(((i + 5) * 2) + 1)))
-
-        material = Material(color=(1., 1., 1.), diffuse=(1., 1., 1.),
-                            specular=(.35, .35, .35))
-        lines = Lines(geometry, material)
-        scene.add(lines)
-
-        self.renderer.render(scene, camera)
-        self.renderer.main_light.intensity = 500
-
-        root.add_widget(self.renderer)
-        self.renderer.bind(size=self._adjust_aspect)
-        return root
-
-    def _adjust_aspect(self, inst, val):
-        rsize = self.renderer.size
-        aspect = rsize[0] / float(rsize[1])
-        self.renderer.camera.aspect = aspect
 
 
 if __name__ == '__main__':
--- kivy3/extras/stlgeometry.py
+++ kivy3/extras/stlgeometry.py
@@ -20,7 +20,6 @@ class STLGeometry(Geometry):
                             stl_mesh.vectors[i][j][2])
                 self.vertices.append(v)
 
-
             f_index = i * 3
             face3 = Face3(f_index, f_index + 1, f_index + 2)
             face3.normal = Vector3(stl_mesh.normals[i][0],
@@ -28,17 +27,3 @@ class STLGeometry(Geometry):
                                    stl_mesh.normals[i][2])
             face3.vertex_normals = [face3.normal, face3.normal, face3.normal]
             self.faces.append(face3)
-
-        # for v in self._cube_vertices:
-        #     v = Vector3(0.5 * v[0] * self.w,
-        #                 0.5 * v[1] * self.h,
-        #                 0.5 * v[2] * self.d)
-        #     self.vertices.append(v)
-
-        # n_idx = 0
-        # for f in self._cube_faces:
-        #     face3 = Face3(*f)
-        #     normal = self._cube_normals[int(n_idx / 2)]
-        #     face3.vertex_normals = [normal, normal, normal]
-        #     n_idx += 1
-        #     self.faces.append(face3)
