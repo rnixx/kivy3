dbf5a68513b2978062ec801fbf873f189be1b0c0
shaunbarlow <shaunbarlowmusic@gmail.com>
2020-01-04 17:58:45 +1100

    Added object movement with mouse based on the plane most close to plane parallel to screen

--- examples/selection/main.py
+++ examples/selection/main.py
@@ -60,9 +60,35 @@ class MainApp(App):
         lines.rotation.x = 90
         scene.add(lines)
 
+        geometry = Geometry()
+        geometry.vertices = [[0.0, 0.0, 0.0], [3.0, 0.0, 0.0]]
+        geometry.lines = [Line2(a=0, b=1)]
+        material = Material(color=(1., 0., 0.), diffuse=(1., 0., 0.),
+                            specular=(.35, .35, .35))
+        lines = Lines(geometry, material)
+        lines.position.y = -0.01
+        scene.add(lines)
+
+        geometry = Geometry()
+        geometry.vertices = [[0.0, 0.0, 0.0], [0.0, 3.0, 0.0]]
+        geometry.lines = [Line2(a=0, b=1)]
+        material = Material(color=(0., 1., 0.), diffuse=(0., 1., 0.),
+                            specular=(1., 1.0, 1.0))
+        lines = Lines(geometry, material)
+        scene.add(lines)
+
+        geometry = Geometry()
+        geometry.vertices = [[0.0, 0.0, 0.0], [0.0, 0.0, 3.0]]
+        geometry.lines = [Line2(a=0, b=1)]
+        material = Material(color=(0., 0., 1.), diffuse=(0., 0., 1.),
+                            specular=(.35, .35, .35))
+        lines = Lines(geometry, material)
+        lines.position.y = -0.01
+        scene.add(lines)
+
         self.renderer.render(scene, camera)
         self.renderer.main_light.intensity = 1000
-        self.renderer.main_light.pos = (10, 1, -10)
+        self.renderer.main_light.pos = (10, 10, -10)
 
         root.add_widget(self.renderer)
         self.renderer.bind(size=self._adjust_aspect)
@@ -98,7 +124,9 @@ class ObjectTrackball(FloatLayout):
     def on_touch_down(self, touch):
         touch.grab(self)
         self._touches.append(touch)
+        self.select_clicked_object(touch)
 
+    def select_clicked_object(self, touch):
         self.renderer.fbo.shader.source = 'select_mode.glsl'
         self.renderer.set_clear_color((0., 0., 0., 0.))
         self.renderer.fbo.ask_update()
@@ -120,9 +148,7 @@ class ObjectTrackball(FloatLayout):
     def on_touch_move(self, touch):
         if touch in self._touches and touch.grab_current == self:
             if self.selected_object:
-                #TODO: Solve for moving the objects in a sensible axis based on camera angle
-                self.selected_object.position.x += (touch.dx * 0.01)
-                self.selected_object.position.y += (touch.dy * 0.01)
+                self.move_object(touch)
             elif len(self._touches) == 1:
                 self.do_rotate(touch)
             elif len(self._touches) == 2:
@@ -133,6 +159,16 @@ class ObjectTrackball(FloatLayout):
         self.phi += d_phi
         self.theta += d_theta
 
+        while self.phi < -180:
+            self.phi += 360
+        while self.phi > 180:
+            self.phi -= 360
+
+        while self.theta < -180:
+            self.theta += 360
+        while self.theta > 180:
+            self.theta -= 360
+
         _phi = math.radians(self.phi)
         _theta = math.radians(self.theta)
         z = self.radius * math.cos(_theta) * math.sin(_phi)
@@ -140,6 +176,127 @@ class ObjectTrackball(FloatLayout):
         y = self.radius * math.cos(_phi)
         self.camera.pos = x, y, z
         self.camera.look_at((0, 0, 0))
+        # print("phi", round(self.phi, 2), "theta", round(self.theta, 2),
+        #       'camera pos', round(x, 2), round(y, 2), round(z, 2))
+
+    def move_object(self, touch):
+        mouse_x = [0, 0, 0]
+        mouse_y = [0, 0, 0]
+
+        if -135 < self.phi < -45:
+            if -135 < self.theta < -45:
+                # print('a -zy')
+                mouse_x = [0, 0, -1]
+                mouse_y = [0, 1, 0]
+            elif -45 < self.theta < 45:
+                # print('b -xy')
+                mouse_x = [-1, 0, 0]
+                mouse_y = [0, 1, 0]
+            elif 45 < self.theta < 135:
+                # print('c zy')
+                mouse_x = [0, 0, 1]
+                mouse_y = [0, 1, 0]
+            else:  # self.theta > 135 or self.theta < -135
+                # print('d xy')
+                mouse_x = [1, 0, 0]
+                mouse_y = [0, 1, 0]
+
+        elif -45 < self.phi < 0:
+            if -135 < self.theta < -45:
+                # print('e -z-x')
+                mouse_x = [0, 0, -1]
+                mouse_y = [-1, 0, 0]
+            elif -45 < self.theta < 45:
+                # print('f -xz')
+                mouse_x = [-1, 0, 0]
+                mouse_y = [0, 0, 1]
+            elif 45 < self.theta < 135:
+                # print('g zx')
+                mouse_x = [0, 0, 1]
+                mouse_y = [1, 0, 0]
+            else:  # self.theta > 135 or self.theta < -135
+                # print('h x-z')
+                mouse_x = [1, 0, 0]
+                mouse_y = [0, 0, -1]
+
+        elif 0 < self.phi < 45:
+            if -135 < self.theta < -45:
+                # print('e2 zx')
+                mouse_x = [0, 0, 1]
+                mouse_y = [1, 0, 0]
+            elif -45 < self.theta < 45:
+                # print('f2 x-z')
+                mouse_x = [1, 0, 0]
+                mouse_y = [0, 0, -1]
+            elif 45 < self.theta < 135:
+                # print('g2 -z-x')
+                mouse_x = [0, 0, -1]
+                mouse_y = [-1, 0, 0]
+            else:  # self.theta > 135 or self.theta < -135
+                # print('h2 -xz')
+                mouse_x = [-1, 0, 0]
+                mouse_y = [0, 0, 1]
+
+        elif 45 < self.phi < 135:
+            if -135 < self.theta < -45:
+                # print('i zy')
+                mouse_x = [0, 0, 1]
+                mouse_y = [0, 1, 0]
+            elif -45 < self.theta < 45:
+                # print('j xy')
+                mouse_x = [1, 0, 0]
+                mouse_y = [0, 1, 0]
+            elif 45 < self.theta < 135:
+                # print('k -zy')
+                mouse_x = [0, 0, -1]
+                mouse_y = [0, 1, 0]
+            else:  # self.theta > 135 or self.theta < -135
+                # print('l -xy')
+                mouse_x = [-1, 0, 0]
+                mouse_y = [0, 1, 0]
+
+        elif self.phi > 135:
+            if -135 < self.theta < -45:
+                # print('m z-x')
+                mouse_x = [0, 0, 1]
+                mouse_y = [-1, 0, 0]
+            elif -45 < self.theta < 45:
+                # print('n xz')
+                mouse_x = [1, 0, 0]
+                mouse_y = [0, 0, 1]
+            elif 45 < self.theta < 135:
+                pass
+                # print('o -zx')
+                mouse_x = [0, 0, -1]
+                mouse_y = [1, 0, 0]
+            else:  # self.theta > 135 or self.theta < -135
+                # print('p -x-z')
+                mouse_x = [-1, 0, 0]
+                mouse_y = [0, 0, -1]
+
+        else:  # self.phi < -135
+            if -135 < self.theta < -45:
+                # print('m2 -zx')
+                mouse_x = [0, 0, -1]
+                mouse_y = [1, 0, 0]
+            elif -45 < self.theta < 45:
+                # print('n2 -x-z')
+                mouse_x = [-1, 0, 0]
+                mouse_y = [0, 0, -1]
+            elif 45 < self.theta < 135:
+                # print('o2 z-x')
+                mouse_x = [0, 0, 1]
+                mouse_y = [-1, 0, 0]
+            else:  # self.theta > 135 or self.theta < -135
+                # print('p2 xz')
+                mouse_x = [1, 0, 0]
+                mouse_y = [0, 0, 1]
+
+        mouse_x = [touch.dx * 0.01 * i for i in mouse_x]
+        mouse_y = [touch.dy * 0.01 * i for i in mouse_y]
+        self.selected_object.position.x += mouse_x[0] + mouse_y[0]
+        self.selected_object.position.y += mouse_x[1] + mouse_y[1]
+        self.selected_object.position.z += mouse_x[2] + mouse_y[2]
 
     def get_selected_object(self, selected_id):
         for obj in self.object_list:
