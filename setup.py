d36598e313bffbd2aa2764e3680186761aa48988
Jean-Luc_BluePrintLab <j.stevens@blueprintlab.com>
2020-04-02 13:11:24 +1100

    update setup.py for version upgrade - test to see if can upgrade from it

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ for root, subFolders, files in walk('examples'):
 
 setup(
     name='kivy3',
-    version='0.1',
+    version='0.2',
     description='Kivy extensions for 3D graphics',
     author='Niko Skrypnik',
     author_email='nskrypnik@gmail.com',
